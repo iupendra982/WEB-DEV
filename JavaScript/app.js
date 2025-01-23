5611f123be9faac6696a37498e34fe9da48b8983
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2025-01-23 11:05:23 +0530

    .

---
+++ JavaScript/app.js
@@ -0,0 +1,29 @@
+// console.log("Hello World!");
+// console.log("Apna College");
+// let a=10;
+// let b=5;
+// console.log("sum is :",a+b)
+// //this is a comment
+
+// let pencilPrice=10;
+// let eraserPrice=5;
+// console.log("The total price is :",pencilPrice+eraserPrice,"Rupees");
+// console.log(`The total Price is : ${pencilPrice+eraserPrice} Rupees`);
+// let age=23;
+// if(age>=18){
+//     console.log("You are eligible to vote");
+// }
+
+// alert("You had to work hard")//only simple alert
+
+let goal =prompt("Enter your goal")//alert with input field
+console.log(goal);
+
+// hardfcomnsole.error("This is normal msg");
+comnsole.warning("This is warning msg");
+
+
+let fname=prompt("Enter first name :");
+let lname=prompt("Enter last name :");
+let msg="Welcome "+fname+" "+lname;
+alert(msg);
