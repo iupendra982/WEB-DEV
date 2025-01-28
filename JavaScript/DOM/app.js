2ecd073ee385a7e3b88a75d898448aa681e8cf0e
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2025-01-29 00:27:16 +0530

    .

---
+++ JavaScript/DOM/app.js
@@ -0,0 +1,20 @@
+//changing the images to difeerent from html file by selcting them by
+//getElementByClassName("class_name");
+
+// let smallImages=document.getElementsByClassName("oldImg");
+
+// for(let i=0;i<smallImages.length;i++){
+//     smallImages[i].src="assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed.`);
+// }
+
+//selcting elements by query selctor
+console.dir(document.querySelector("h1"));
+//selecting all elments
+console.dir(document.querySelectorAll("div a"));
+
+//manipulating style using style property
+let links=document.querySelectorAll(".box a");
+for(link of links){
+    link.style.color="red";
+}
