2a718caa59a25f5c5ba1efa417fad7337acf1071
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2025-02-08 23:41:28 +0530

    .

--- JavaScript/form_events.js
+++ JavaScript/form_events.js
@@ -1,6 +1,11 @@
 let form=document.querySelector("form");
 
 form.addEventListener("submit",function(event){
-    event.preventDefault();
+    event.preventDefault();//here we are stopping the default action of the 
+    //form to open the url associated with submit button
     alert("Form submitted");
+
+    let inp=document.querySelector("input");
+    console.dir(inp);
+    console.log(inp.value);
 });
