445de76cca4cd13cd9822d0ace8f6169744c783a
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2025-03-10 22:54:20 +0530

    .

--- JavaScript/simon_game.js
+++ JavaScript/simon_game.js
@@ -12,7 +12,6 @@ document.addEventListener("keypress",function(){
     if(started==false){
         console.log("Game Started");
         started=true;
-
         levelup();
     }
 });
@@ -21,10 +20,18 @@ function btnFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
-    },250);
+    },350);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },200);
 }
 
 function levelup(){
+    userSeq=[];
     level++;
     h2.innerText=`Level ${level}`;
 
@@ -32,8 +39,45 @@ function levelup(){
     let randIdx=Math.floor(Math.random()*3);
     let randColor=btns[randIdx];
     let randBtn=document.querySelector(`.${randColor}`);
-    conasole.log(randIdx);
-    conasole.log(randColor);
-    conasole.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
     btnFlash(randBtn);
+}
+
+function checkAns(idx){
+    if(userSeq[idx]===gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+           setTimeout(levelup(),2000); 
+        }
+    }
+    else{
+        h2.innerHTML=`Game over! Your score was <b>${level}</b> <br> Press any key to start`;
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor="white";
+        },150);
+        reset();
+    };
+};
+
+function btnPress(){
+    let btn=this;
+    userFlash(btn);
+
+    userColor=btn.getAttribute("id");
+    userSeq.push(userColor);
+    console.log(`User sequence : ${userSeq}`);
+    checkAns(userSeq.length-1);
+};
+
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
 }
