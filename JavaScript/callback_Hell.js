277b51cf82512960eb413f208dcb7b76658aac9f
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2025-03-11 22:59:52 +0530

    .

--- JavaScript/callback_Hell.js
+++ JavaScript/callback_Hell.js
@@ -20,17 +20,61 @@
 //     });
 // });
 
-function saveToDb(data,success,failure){
+// function saveToDb(data,success,failure){
+//     let interenetSpeed=Math.floor(Math.random()*10)+1;
+//     if(interenetSpeed>4){
+//         success();
+//     }else{
+//         failure();
+//     }
+// }
+
+// saveToDb("Apna COllege",()=>{
+//     console.log("Success : Data saved to DB");
+//     saveToDb("hello world ",()=>{
+//         console.log("Success2 : data2 saved ");
+//     },()=>{
+//         console.log("Failure2 : Weak Connection")
+//     })
+// },()=>{
+//     console.log("Failure : Error saving data");
+// });
+
+function saveToDb(data){
     let interenetSpeed=Math.floor(Math.random()*10)+1;
-    if(interenetSpeed>4){
-        success();
-    }else{
-        failure();
-    }
+    return new Promise((resolve,reject)=>{
+        if(interenetSpeed>4){
+            resolve("Success : data was saved");
+        }else{
+            reject("Failure : Weak Connection");
+        }
+    });
 }
 
-saveToDb("Apna COllege",()=>{
-    console.log("Data saved : ");
-},()=>{
-    console.log("Error saving data");
+// let request=saveToDb("apna college");//req=promise object
+// request.then(()=>{
+//     console.log("Promise was resolved");
+//     console.log(request);
+// })
+// .catch(()=>{
+//     console.log("Promise was rejected");
+//     console.log(request);
+// });
+
+//compact way of writing is 
+
+saveToDb("Data1")
+.then(()=>{
+    console.log("Data1 saved :Promise was resolved");
+     return saveToDb("Data2");
+})
+.then(()=>{
+    console.log("Data2 saved :Promise was resolved");
+    return saveToDb("Data3");
+})
+.then(()=>{
+    console.log("Data3 saved :Promise was resolved");
+})
+.catch(()=>{
+    console.log("Promise was rejected");
 });
