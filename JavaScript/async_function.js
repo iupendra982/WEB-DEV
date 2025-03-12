4ce63988a810770148b4e1fe2fadd33b9ee86487
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2025-03-12 17:55:04 +0530

    .

--- JavaScript/async_function.js
+++ JavaScript/async_function.js
@@ -1,17 +1,57 @@
-async function greet() {
-    throw "404 page not found";//to throw an error
-    return "hello!";//returns a promise
+// async function greet() {
+//     throw "404 page not found";//to throw an error
+//     return "hello!";//returns a promise
+// }
+
+// greet()
+// .then((result)=>{
+//     console.log("Promise was resolved");
+//     console.log("Result was : " , result);
+// })
+// .catch((error)=>{
+//     console.log("Promise was rejected : ",error);
+// });
+
+// let demo= async ()=>{
+//     return 5;
+// };
+
+
+//what it does is until one promise is not fullfilled new one is not proceeded
+function getNum(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let num = Math.floor(Math.random()*10);
+            console.log(num);
+            resolve();
+        },1000);
+    });
 }
 
-greet()
-.then((result)=>{
-    console.log("Promise was resolved");
-    console.log("Result was : " , result);
-})
-.catch((error)=>{
-    console.log("Promise was rejected : ",error);
-});
+async function demo(){
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+}
 
-let demo= async ()=>{
-    return 5;
-};
+//we can imply this to our color problem
+h1=document.querySelector("h1");
+
+function changeColor(color,delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            h1.style.color=color;
+            console.log(`Color changed to ${color}`);
+            resolve("color changed!");
+        },delay);
+    });
+}
+
+async function changeColorDemo(){
+    await changeColor("red",1000);
+    await changeColor("orange",1000);
+    await changeColor("blue",1000);
+    await changeColor("green",1000);
+    await changeColor("yellow",1000);
+    }
