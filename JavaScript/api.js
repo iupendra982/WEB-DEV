854ec9134fd4c12e9fadef6f00200452a4ea0bab
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2025-03-13 19:14:21 +0530

    .

---
+++ JavaScript/api.js
@@ -0,0 +1,46 @@
+// let jsonRes='{"fact":"A cat\u2019s hearing is better than a dog\u2019s. And a cat can hear high-frequency sounds up to two octaves higher than a human.","length":119}';
+
+// let validRes=JSON.parse(jsonRes);
+// console.log(validRes);
+// console.log(validRes.fact);
+
+// let student={
+//     name:"John",
+//     age:20,
+// };
+// console.log(JSON.stringify(student));//it is json data
+
+
+//Requesting url
+let url="https://catfact.ninja/fact";
+fetch(url)//it returns a promise
+.then((response)=>{
+    console.log(response);
+    return response.json();//getting the data
+})
+.then((data)=>{
+    console.log("Data1 : ",data.fact);
+    return fetch(url);
+})
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log("Data2 :",data.fact);
+})
+.catch((err)=>{
+    console.log("Error : ",err);
+})
+
+//using async and await
+//sometimes response can be delayed and result in promise will show undefined
+//async and await will wait for the response and then execute the next line of code
+async function getCatFact(){
+    try{
+        let res=await fetch(url);
+        let data=await res.json();
+        console.log(data);
+    }catch(e){
+        console.log("Error : ",e);
+    }
+}
--- JavaScript/async_function.js
+++ JavaScript/async_function.js
@@ -21,7 +21,7 @@
 function getNum(){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            let num = Math.floor(Math.random()*10);
+            let num = Math.floor(Math.random()*10)+1;
             console.log(num);
             resolve();
         },1000);
@@ -41,6 +41,10 @@ h1=document.querySelector("h1");
 function changeColor(color,delay){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
+            let num = Math.floor(Math.random()*5)+1;
+            if(num>3){
+                reject("Promise rejected");
+            }
             h1.style.color=color;
             console.log(`Color changed to ${color}`);
             resolve("color changed!");
@@ -49,9 +53,18 @@ function changeColor(color,delay){
 }
 
 async function changeColorDemo(){
-    await changeColor("red",1000);
-    await changeColor("orange",1000);
-    await changeColor("blue",1000);
-    await changeColor("green",1000);
-    await changeColor("yellow",1000);
+    try{
+        await changeColor("red",1000);
+        await changeColor("orange",1000);
+        await changeColor("blue",1000);
+        await changeColor("green",1000);
+        await changeColor("yellow",1000);
+    }catch(err){
+        console.log("Error caught");
+        console.log(err);
     }
+
+    let a=5;
+    console.log(a);
+    console.log("New number = ",a+3);
+}
